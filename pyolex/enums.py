af02ddc740ce4368afa4c391c988667ad20f1adb
Joakim Skjefstad <skjefstad.joakim@gmail.com>
2023-02-03 22:15:05 +0100

    Refactoring a bit

---
+++ pyolex/enums.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from enum import Enum
+
+class Symbol(Enum): # Should be 23 in total
+    BROWNCIRCLE = "Brunsirkel"
+    YELLOWDANGER = "Gulfare"
+    FISHINGNET_START = "Garnstart"
+    FISHINGNET_STOP = "Garnstopp"
+
+class Color(Enum): # Can we make these without Norwegian letters ø and å?
+    RED = "Rød"
+    BLUE = "Blå"
+    GREEN = "Grønn"
+    PURPLE = "Lilla"
+    BROWN = "Brun"
+    BLACK = "Svart"
+
+class RouteType(Enum):
+    DEFAULT = "uten navn"
+    QUICKCROSS = "Hurtigkryss"
+    TOWLINE = "Slepestrek"
---
+++ pyolex/olex.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# Class for Olex file format
+
+from .utilities import *
+
+class Olex:
+    def __init__(self):
+        self._olex = True
+        print("Olex object made")
